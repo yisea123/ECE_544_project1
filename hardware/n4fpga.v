368a03b7e7ae9f1d7c443bba425ad51a6cb411d8
Rehan Iqbal <rsi7.unc@gmail.com>
2016-01-23 13:49:28 -0800

    Updated commenting for n4fpga.v

--- hardware/n4fpga.v
+++ hardware/n4fpga.v
@@ -1,145 +1,197 @@
-`timescale 1ns / 1ps
 // n4fpga.v - Top level module for the ECE 544 Getting Started project
 //
-// Copyright Roy Kravitz, Portland State University 2013-2015, 2016
 //
-// Created By:	Roy Kravitz
-// Date:		31-December-2014
-// Version:		1.0
+// Author:	Roy Kravitz
+// Modified by:	Rehan Iqbal
+// Organization: Portland State University
 //
 // Description:
-// ------------
+// 
 // This module provides the top level for the Getting Started hardware.
 // The module assume that a PmodCLP is plugged into the JA and JB
 // expansion ports and that a PmodENC is plugged into the JD expansion 
 // port (bottom row).  
+//
 //////////////////////////////////////////////////////////////////////
-module n4fpga(
-    input				clk,			// 100Mhz clock input
-    input				btnC,			// center pushbutton
-    input				btnU,			// UP (North) pusbhbutton
-    input				btnL,			// LEFT (West) pushbutton
-    input				btnD,			// DOWN (South) pushbutton  - used for system reset
-    input				btnR,			// RIGHT (East) pushbutton
-	input				btnCpuReset,	// CPU reset pushbutton
-    input	[15:0]		sw,				// slide switches on Nexys 4
-    output	[15:0] 		led,			// LEDs on Nexys 4   
-    output              RGB1_Blue,      // RGB1 LED (LD16) 
-    output              RGB1_Green,
-    output              RGB1_Red,
-    output              RGB2_Blue,      // RGB2 LED (LD17)
-    output              RGB2_Green,
-    output              RGB2_Red,
-    output [7:0]        an,             // Seven Segment display
-    output [6:0]        seg,
-    output              dp,
+
+`timescale 1ns / 1ps
+
+module n4fpga (
+
+    /******************************************************************/
+    /* Top-level port declarations                                    */
+    /******************************************************************/
+
+    input               clk,                    // 100Mhz clock from on-board oscillator
+
+    // Pushbuttons & switches
+
+    input               btnC,                   // center pushbutton
+    input               btnU,                   // up (North) pushbutton
+    input               btnL,                   // left (West) pushbutton
+    input               btnD,                   // down (South) pushbutton
+    input               btnR,                   // right (East) pushbutton
+    input               btnCpuReset,            // CPU reset pushbutton
+    input   [15:0]      sw,                     // Nexys4 on-board slide switches
+
+    // LEDs & 7-segment display
+
+    output	[15:0] 		led,			        // Nexys4 on-board LEDs   
+    output  [7:0]       an,                     // 7-segment display anodes
+    output  [6:0]       seg,                    // 7-segment display segments 
+    output              dp,                     // 7-segment display decimal points 
     
-    input				uart_rtl_rxd,	// USB UART Rx and Tx on Nexys 4
-    output				uart_rtl_txd,	
+    // RGB LEDs
+
+    output              RGB1_Blue,              // RGB1 LED (RefDes: LD16) 
+    output              RGB1_Green,             // RGB1 LED (RefDes: LD16)
+    output              RGB1_Red,               // RGB1 LED (RefDes: LD16)
+    output              RGB2_Blue,              // RGB2 LED (RefDes: LD17)
+    output              RGB2_Green,             // RGB2 LED (RefDes: LD17) 
+    output              RGB2_Red,               // RGB2 LED (RefDes: LD17)
+
+    // UART serial port (19200 baud)
+
+    input				uart_rtl_rxd,	        // USB UART Rx
+    output				uart_rtl_txd,	        // USB UART Tx
     
-    output	[7:0] 		JA,				// JA Pmod connector - PmodCLP data bus
-										// both rows are used
-    output	[7:0] 		JB,				// JB Pmod connector - PmodCLP control signals
-										// only the bottom row is used
-    output	[7:0] 		JC,				// JC Pmod connector - debug signals
-										// only the bottom row is used
-	input	[7:0]		JD				// JD Pmod connector - PmodENC signals
-);
+    // Pmod connectors
 
-// internal variables
-wire				sysclk;
-wire				sysreset_n, sysreset;
-wire				rotary_a, rotary_b, rotary_press, rotary_sw;
-wire	[7:0]		lcd_d;
-wire				lcd_rs, lcd_rw, lcd_e;
+    output	[7:0] 		JA,		                // PmodCLP data bus (both rows used)
+    output	[7:0] 		JB,				        // PmodCLP control signals (bottom row only)
+    output	[7:0] 		JC,                     // debug signals (bottom row only)
+	input	[7:0]		JD);                    // PmodENC signals
 
-wire	[7:0]	gpio_in;				// embsys GPIO input port
-wire	[7:0]	gpio_out;				// embsys GPIO output port
-wire            pwm_out;                // PWM output from the axi_timer
+    /******************************************************************/
+    /* Local parameters and variables                                 */
+    /******************************************************************/
 
-// make the connections to the GPIO port.  Most of the bits are unused in the Getting
-// Started project but GPIO's provide a convenient way to get the inputs and
-// outputs from logic you create to and from the Microblaze.  For example,
-// you may decide that using an axi_gpio peripheral is a good way to interface
-// your hardware pulse-width detect logic with the Microblaze.  Our application
-// is simple.
+    // Global signals
 
-//wrap the pwm_out from the timer back to the application program for software pulse-width detect
-assign gpio_in = {7'b0000000, pwm_out};
+    wire				sysclk;                 // 100MHz system clock
+    wire				sysreset_n;             // active-low reset signal for Microblaze
+    wire                sysreset;               // active-high reset signal for any logic blocks
 
-// The FIT interrupt routine synthesizes a 20KHz signal and makes it
-// available on GPIO2[0].  Bring it to the top level as an
-// indicator that the system is running...could be handy for debug
-wire            clk_20khz;
-assign clk_20khz = gpio_out[0];
+    // Connections between Pmod JD <--> rotary encoder
 
-// We want to show the PWM output on LD15 (the leftmost LED;  The higher the PWM
-// duty cycle, the brighter the LED but, the LEDs are controlled by the 
-// Microblaze through Nexys4.  So, we have to play some games.  Instead of
-// driving the leds directly, we will write 0 to led[15] and then OR in the PWM
-// output. 
-wire    [15:0]  led_int;                // Nexys4IO drives these outputs
-assign led = {(pwm_out | led_int[15]), led_int[14:0]};  // LEDs are driven by led
+    wire				rotary_a;               // quadrature-encoded A input from encoder
+    wire                rotary_b;               // quadrature-encoded B input from encoder
+    wire                rotary_press;           // debounced pushbutton from encoder; stored in ROTLCD_STS register
+    wire                rotary_sw;              // debounced slide switch from encoder; stored in ROTLCD_STS register
 
-// make the connections
+    // Connections between Pmod JA/JB/JC <--> LCD display
 
-// system-wide signals
-assign sysclk = clk;
-assign sysreset_n = btnCpuReset;		// The CPU reset pushbutton is asserted low.  The other pushbuttons are asserted high
-										// but the microblaze for Nexys 4 expects reset to be asserted low
-assign sysreset = ~sysreset_n;			// Generate a reset signal that is asserted high for any logic blocks expecting it.
+    wire	[7:0]		lcd_d;                  // 8-bit data bus to the display
+    wire				lcd_rs;                 // Register select: high for data transfer, low for instruction register
+    wire                lcd_rw;                 // Read/write signal: high for read mode, low for write mode
+    wire                lcd_e;                  // Read/write strobe: high for read OE; falling edge writes data
 
-// PmodCLP signals
-// JA - top and bottom rows
-// JB - bottom row only
-assign JA = lcd_d[7:0];
-assign JB = {1'b0, lcd_e, lcd_rw, lcd_rs, 2'b00, clk_20khz, pwm_out};
+    // Connections between AXI Timer <--> GPIO
 
-// PmodENC signals
-// JD - bottom row only
-// Pins are assigned such that turning the knob to the right
-// causes the rotary count to increment.
-assign rotary_a = JD[5];
-assign rotary_b = JD[4];
-assign rotary_press = JD[6];
-assign rotary_sw = JD[7];
+    wire	[7:0]	    gpio_in;				// GPIO input port for EMBSYS
+    wire	[7:0]	    gpio_out;				// GPIO output port for EMBSYS
+    wire                pwm_out;                // AXI Timer PWM --> GPIO input
 
-// Debug signals are on both rows of JC
-assign JC = {lcd_e, lcd_rs, lcd_rw, 1'b0, lcd_d[3:0]}; 
-			
-// instantiate the embedded system
-system EMBSYS
-       (.PmodCLP_DataBus(lcd_d),
-        .PmodCLP_E(lcd_e),
-        .PmodCLP_RS(lcd_rs),
-        .PmodCLP_RW(lcd_rw),
-        .PmodENC_A(rotary_a),
-        .PmodENC_B(rotary_b),
-        .PmodENC_BTN(rotary_press),
-        .PmodENC_SWT(rotary_sw),
-        .RGB1_Blue(RGB1_Blue),
-        .RGB1_Green(RGB1_Green),
-        .RGB1_Red(RGB1_Red),
-        .RGB2_Blue(RGB2_Blue),
-        .RGB2_Green(RGB2_Green),
-        .RGB2_Red(RGB2_Red),
-        .an(an),
-        .btnC(btnC),
-        .btnD(btnD),
-        .btnL(btnL),
-        .btnR(btnR),
-        .btnU(btnU),
-        .dp(dp),
-        .led(led_int),
-        .seg(seg),
-        .sw(sw),
-        .sysreset_n(sysreset_n),
-        .sysclk(sysclk),
-        .uart_rtl_rxd(uart_rtl_rxd),
-        .uart_rtl_txd(uart_rtl_txd),
-        .gpio_0_GPIO2_tri_o(gpio_out),
-        .gpio_0_GPIO_tri_i(gpio_in),
-        .pwm0(pwm_out));
+    /******************************************************************/
+    /* Global Assignments                                             */
+    /******************************************************************/
+
+    // global signals
+
+    assign sysclk = clk;
+    assign sysreset_n = btnCpuReset;        // active-low reset signal for Microblaze
+    assign sysreset = ~sysreset_n;          // active-high reset signal for any logic blocks
+
+    // 20kHz signal from FIT interrupt routine (used for debugging)
+
+    wire   clk_20khz;
+    assign clk_20khz = gpio_out[0];
+
+    // PWM output on led[15]; Microblaze controls LEDs via Nexys4IO block
+    // so we write '0' and OR with PWM output
+    
+    wire   [15:0]      led_int;                                    // Nexys4IO drives these outputs
+    assign led = {(pwm_out | led_int[15]), led_int[14:0]};         // LEDs are driven by led
+
+    // output LCD signals to ports JA/JB/JC
+
+    assign JA = lcd_d[7:0];                                                     // 8-bit data bus (both rows used)
+    assign JB = {1'b0, lcd_e, lcd_rw, lcd_rs, 2'b00, clk_20khz, pwm_out};       // control signals (bottom row only)
+    assign JC = {lcd_e, lcd_rs, lcd_rw, 1'b0, lcd_d[3:0]};                      // debug signals (bottom row only)
+
+    // input rotary signals from port JD
+
+    assign rotary_a = JD[5];                // quadrature-encoded A input from encoder
+    assign rotary_b = JD[4];                // quadrature-encoded B input from encoder
+    assign rotary_press = JD[6];            // pushbutton from encoder; stored in ROTLCD_STS register
+    assign rotary_sw = JD[7];               // slide switch from encoder; stored in ROTLCD_STS register
+
+    // wrap the pwm_out from the timer back to the application for software pulse-width detect
+
+    assign gpio_in = {7'b0000000, pwm_out};
+    			
+    /******************************************************************/
+    /* EMBSYS instantiation                                           */
+    /******************************************************************/
+
+    system EMBSYS (
+
+        // Global signals
+        
+        .sysreset_n                 (sysreset_n),       // I [ 0 ] active-low reset signal for Microblaze
+        .sysclk                     (sysclk),           // I [ 0 ] 100MHz system clock
+        
+        // Connections with LCD display
+
+        .PmodCLP_DataBus            (lcd_d),            // O [7:0] 8-bit data bus to the display
+        .PmodCLP_E                  (lcd_e),            // O [ 0 ] Read/write strobe: high for read OE; falling edge writes data
+        .PmodCLP_RS                 (lcd_rs),           // O [ 0 ] Register select: high for data transfer, low for instruction register
+        .PmodCLP_RW                 (lcd_rw),           // O [ 0 ] Read/write signal: high for read mode, low for write mode
+
+        // Connections with rotary encoder
+
+        .PmodENC_A                  (rotary_a),         // I [ 0 ] quadrature-encoded A input from encoder
+        .PmodENC_B                  (rotary_b),         // I [ 0 ] quadrature-encoded B input from encoder
+        .PmodENC_BTN                (rotary_press),     // I [ 0 ] debounced pushbutton from encoder; stored in ROTLCD_STS register
+        .PmodENC_SWT                (rotary_sw),        // I [ 0 ] debounced slide switch fromencoder; stored in ROTLCD_STS register
+
+        // Connections with RGB LEDs
+
+        .RGB1_Blue                  (RGB1_Blue),        // O [ 0 ] tri-color LED (RefDes: LD16)
+        .RGB1_Green                 (RGB1_Green),       // O [ 0 ] tri-color LED (RefDes: LD16)
+        .RGB1_Red                   (RGB1_Red),         // O [ 0 ] tri-color LED (RefDes: LD16)
+        .RGB2_Blue                  (RGB2_Blue),        // O [ 0 ] tri-color LED (RefDes: LD17)
+        .RGB2_Green                 (RGB2_Green),       // O [ 0 ] tri-color LED (RefDes: LD17)
+        .RGB2_Red                   (RGB2_Red),         // O [ 0 ] tri-color LED (RefDes: LD17)
+
+        // Connections with pushbuttons & switches
+
+        .btnC                       (btnC),             // I [ 0 ]  center pushbutton input
+        .btnD                       (btnD),             // I [ 0 ]  down pushbutton input
+        .btnL                       (btnL),             // I [ 0 ]  left pushbutton input
+        .btnR                       (btnR),             // I [ 0 ]  right pushbutton input 
+        .btnU                       (btnU),             // I [ 0 ]  up pushbutton input
+        .sw                         (sw),               // I [15:0] slide switch inputs
+
+        // Connections with LEDs & 7-segment display
+
+        .dp                         (dp),               // O [ 0 ]  7-segment display decimal points
+        .an                         (an),               // O [7:0]  7-segment display anodes
+        .seg                        (seg),              // O [6:0]  7-segment display segments
+        .led                        (led_int),          // O [15:0] Nexys4 on-board LEDs
+
+        // Connections with UART
+
+        .uart_rtl_rxd               (uart_rtl_rxd),     // I [ 0 ] USB UART Rx (19200 baud)
+        .uart_rtl_txd               (uart_rtl_txd),     // O [ 0 ] USB UART Tx (19200 baud)
+
+        // Connections with GPIO
+
+        .gpio_0_GPIO2_tri_o         (gpio_out),         // O [7:0] GPIO output port; AXI Timer 'clk_20khz' --> bit[0]
+        .gpio_0_GPIO_tri_i          (gpio_in),          // I [7:0] GPIO input port; AXI Timer 'pwm_out' --> bit[0]
+
+        // Connections with AXI Timer
+
+        .pwm0                       (pwm_out));         // O [ 0 ] AXI Timer's PWM output signal
 
 endmodule
-
